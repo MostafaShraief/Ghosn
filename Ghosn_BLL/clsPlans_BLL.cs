39df1927870ce9bef898dbbc5378d2777ff99734
Mostafa Shraeif <mostafashraief5@gmail.com>
2025-03-15 03:07:12 +0300

    add clsPeople + clsPlans

---
+++ Ghosn_BLL/clsPlans_BLL.cs
@@ -0,0 +1,68 @@
+﻿using Ghosn_DAL;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghosn_BLL
+{
+    public class PlanDTO
+    {
+        public int PlanID { get; set; }
+        public int ClientID { get; set; }
+        public int InputID { get; set; }
+        public int OutputID { get; set; }
+    }
+
+    public class clsPlans_BLL
+    {
+        public static List<PlanDTO> GetAllPlans()
+        {
+            var plansObjects = clsPlans_DAL.GetAllPlans();
+            return plansObjects.Select(ConvertToDTO).ToList();
+        }
+
+        public static PlanDTO? GetPlanById(int id)
+        {
+            var planObject = clsPlans_DAL.GetPlanById(id);
+            return planObject != null ? ConvertToDTO(planObject) : null;
+        }
+
+        public static int AddPlan(PlanDTO dto)
+        {
+            var planObject = ConvertToDALObject(dto);
+            return clsPlans_DAL.AddPlan(planObject);
+        }
+
+        public static bool UpdatePlan(PlanDTO dto)
+        {
+            var planObject = ConvertToDALObject(dto);
+            return clsPlans_DAL.UpdatePlan(planObject);
+        }
+
+        public static bool DeletePlan(int id)
+        {
+            return clsPlans_DAL.DeletePlan(id);
+        }
+
+        // Conversion methods
+        private static PlanDTO ConvertToDTO(PlanObject obj)
+        {
+            return new PlanDTO
+            {
+                PlanID = obj.PlanID,
+                ClientID = obj.ClientID,
+                InputID = obj.InputID,
+                OutputID = obj.OutputID
+            };
+        }
+
+        private static PlanObject ConvertToDALObject(PlanDTO dto)
+        {
+            return new PlanObject(dto.PlanID, dto.ClientID, dto.InputID, dto.OutputID);
+        }
+    }
+}
---
+++ Ghosn_DAL/clsPeople_DAL.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghosn_DAL
+{
+    public class clsPeople_DAL
+    {
+        private static string _connectionString = clsSettings.connectionString;
+
+        public class PersonObject
+        {
+            public int PersonID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+
+            public PersonObject(int personID, string firstName, string lastName, string email)
+            {
+                PersonID = personID;
+                FirstName = firstName;
+                LastName = lastName;
+                Email = email;
+            }
+        }
+
+        public static List<PersonObject> GetAllPeople()
+        {
+            var people = new List<PersonObject>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM People";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            people.Add(new PersonObject(
+                                reader.GetInt32("PersonID"),
+                                reader.GetString("FirstName"),
+                                reader.GetString("LastName"),
+                                reader.GetString("Email")
+                            ));
+                        }
+                    }
+                }
+            }
+            return people;
+        }
+
+        public static PersonObject? GetPersonById(int personId)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM People WHERE PersonID = @PersonID";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@PersonID", personId);
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new PersonObject(
+                                reader.GetInt32("PersonID"),
+                                reader.GetString("FirstName"),
+                                reader.GetString("LastName"),
+                                reader.GetString("Email")
+                            );
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public static int AddPerson(PersonObject person)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO People (FirstName, LastName, Email) VALUES (@FirstName, @LastName, @Email); SELECT SCOPE_IDENTITY();";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", person.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", person.LastName);
+                    cmd.Parameters.AddWithValue("@Email", person.Email);
+                    conn.Open();
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+        }
+
+        public static bool UpdatePerson(PersonObject person)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE People SET FirstName = @FirstName, LastName = @LastName, Email = @Email WHERE PersonID = @PersonID";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@PersonID", person.PersonID);
+                    cmd.Parameters.AddWithValue("@FirstName", person.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", person.LastName);
+                    cmd.Parameters.AddWithValue("@Email", person.Email);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+        public static bool DeletePerson(int personId)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM People WHERE PersonID = @PersonID";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@PersonID", personId);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+    }
+}
